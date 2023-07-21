1964974e4f404a67952be02b3a7c491d3fc82b32
joao.calca <desenvolvimento.joaocalca@gmail.com>
2023-07-21 16:20:21 -0300

    Login, JWT token

--- hr-oauth/src/main/java/com/devsuperior/hroauth/config/AppConfig.java
+++ hr-oauth/src/main/java/com/devsuperior/hroauth/config/AppConfig.java
@@ -1,21 +1,33 @@
 package com.devsuperior.hroauth.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
+import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 
-@EnableWebSecurity
+@Configuration
+@Order(100)
 public class AppConfig extends WebSecurityConfigurerAdapter {
 
-    @Override
-    public void configure(HttpSecurity http) throws Exception {
-        // @formatter:off
-        http.csrf()
-                .disable()
-                .exceptionHandling()
-                .and()
-                .authorizeRequests()
-                .antMatchers("/hr-oauth/**").permitAll();
-        // @formatter:on
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public JwtAccessTokenConverter accessTokenConverter(){
+        JwtAccessTokenConverter tokenConverter = new JwtAccessTokenConverter();
+        tokenConverter.setSigningKey("MY-SECRET-KEY");
+        return tokenConverter;
+    }
+
+    @Bean
+    public JwtTokenStore tokenStore(){
+        return new JwtTokenStore(accessTokenConverter());
     }
 }
